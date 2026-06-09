54e78cd37dfffb14b8d559336ab3014db9a61389
theinklogic-maker <theinklogic@gmail.com>
2026-06-09 14:44:27 -0300

    Create PagoInfoCarService.kt

---
+++ app/src/main/java/com/stuch/pagoinfo/service/PagoInfoCarService.kt
@@ -0,0 +1,16 @@
+package com.stuch.pagoinfo.service
+
+import androidx.car.app.CarAppService
+import androidx.car.app.Session
+import androidx.car.app.validation.HostValidator
+
+class PagoInfoCarService : CarAppService() {
+
+    override fun createHostValidator(): HostValidator {
+        return HostValidator.ALLOW_ALL_HOSTS_VALIDATOR
+    }
+
+    override fun onCreateSession(): Session {
+        return PagoInfoSession()
+    }
+}
