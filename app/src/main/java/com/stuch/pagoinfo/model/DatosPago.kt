ee424bedb49dd39151a2b1b9ff5dba418c86a361
theinklogic-maker <theinklogic@gmail.com>
2026-06-10 23:40:17 -0300

    Update DatosPago.kt

--- app/src/main/java/com/stuch/pagoinfo/model/DatosPago.kt
+++ app/src/main/java/com/stuch/pagoinfo/model/DatosPago.kt
@@ -4,5 +4,6 @@ data class DatosPago(
     val alias: String = "",
     val titular: String = "",
     val banco: String = "",
-    val mensajeTTS: String = "Buen día, gracias por su viaje. El alias de pago es: "
+    val mensajeTTS: String = "",
+    val vozNombre: String = ""
 )
